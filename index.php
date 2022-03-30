29fe6424d032d17e48ef0c37c3f03c2707e99f1e
Guillem Talayero Carrasco <Guillem>
2022-03-30 12:38:13 +0200

    first commit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootrstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- hoja de estilos -->
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <!-- título de página -->
     <title>Título de la página</title>
     <!-- ícono de pàgina -->
@@ -28,23 +28,26 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href=".">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
         <?php
         $aux=[];
             foreach($films->film as $film){
                 if(!in_array((string)$film['cine'],$aux)){
                 echo '<li class="nav-item">';
-                echo '<a class="nav-link active" aria-current="page" href="#">'.$film['cine'].'</a>';
+                if(isset($_GET['cine']) && $_GET['cine']==(string)$film['cine']){
+                    echo '<a class="nav-link active" aria-current="page" href="?cine='.$film['cine'].'">'.$film['cine'].'</a>';
+                }else{
+                  echo '<a class="nav-link" aria-current="page" href="?cine='.$film['cine'].'">'.$film['cine'].'</a>';
+                }
                 echo '</li>';
+
                 array_push($aux,(string)$film['cine']);
+
                 }
 
                 
@@ -56,23 +59,64 @@
     </div>
   </div>
 </nav>
-    <div class="row">
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/img1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/img2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/img3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    <div class="row gx-0">
         <div class="column-1">
-            <?php
-                if (file_exists('xml/encartelera.xml')) {
-                    $films = simplexml_load_file('xml/encartelera.xml');
-                    foreach($films->film as $film){
-                        echo $film->title.' - ';
-                        /* Imprimir el contenido del atriburo tema */
-                        echo $film->description['tema'].'<br>';
-                    }
-                    
-                    
-                    /* print_r($xml); */
-                } else {
-                    exit('Error abriendo test.xml.');
-                }
-            ?>
+        <table class="table table-striped table-hover table-bordered ">
+  <thead>
+    <tr class=" table-light border-dark">
+      <th scope="col">Pelicula</th>
+      <th class="column-desc" scope="col">Descripcion</th>
+      <th scope="col">Tema</th>
+    </tr>
+  </thead>
+  <tbody class="table-light border-dark">
+  <?php
+    if(isset($_GET['cine'])){
+        /* filtro por cine */
+        foreach($films->film as $film){
+            if($_GET['cine']==$film['cine']){
+                echo '<tr>';
+                echo '<td>'.$film->title.'</td>';
+                echo '<td class="column-desc">'.$film->description.'</td>';
+                echo '<td>'.$film->description['tema'].'</td>';
+                echo '</tr>';
+            }
+        }
+    }else{
+        /* no filtro (muestro todos los datos) */
+        foreach($films->film as $film){
+            echo '<tr>';
+            echo '<td>'.$film->title.'</td>';
+            echo '<td class="column-desc">'.$film->description.'</td>';
+            echo '<td>'.$film->description['tema'].'</td>';
+            echo '</tr>';
+        }
+    }
+    ?>
+  </tbody>
+</table>
+            
         </div>
     </div>
 </body>
